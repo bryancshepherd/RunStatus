b318f4465e730eb8ac183b490e67b040e1ab4e02
Bryan Shepherd <bryan.shepherd@windsorcircle.com>
2016-04-20 19:40:36 -0400

    Minor wording change.

--- main.R
+++ main.R
@@ -13,6 +13,6 @@ session_key <- getUserKey(api_dk, api_un, api_up)
 pbPost(session_key = session_key, 
        api_dev_key = api_dk, 
        text = "Pasted from R", 
-       title = "Take me out", 
+       title = "In Progress", 
        expire = "10M",
        privacy_code = "0")
