511fa7e228ddfacd000bd25fa19117712d526956
Bryan Shepherd <bryan.c.shepherd@gmail.com>
2016-04-22 22:07:43 -0500

    Add light functions to get_status.py

--- get_status.py
+++ get_status.py
@@ -27,3 +27,42 @@ print(my_key)
 pastes = pb.pastes_by_user(api_dev_key = api_dk, api_user_key = my_key.decode('utf-8'), results_limit = 1)
 
 test = re.search('<paste_title>(?s)(.*)</paste_title>', 'asdf<paste_title>Hello7world</paste_title>asdf')
+
+## Useful resources
+# https://learn.sparkfun.com/tutorials/resistors/example-applications
+# https://learn.sparkfun.com/tutorials/light-emitting-diodes-leds
+# https://en.wikipedia.org/wiki/LED
+
+# The RPi/Python/LED interface
+import RPi.GPIO as GPIO ## Import GPIO library
+GPIO.setmode(GPIO.BCM) ## Use internal pin numbering
+
+# Initialize LEDs
+# Green light
+GPIO.setup(22, GPIO.OUT) ## Setup GPIO pin 25 to OUT
+
+# Initialize Pulse width modulation on GPIO 25. Frequency=100Hz and OFF
+pG = GPIO.PWM(22, 100)
+pG.start(0)
+
+# Red light
+GPIO.setup(25, GPIO.OUT) ## Setup GPIO pin 22 to OUT
+
+# Initialize Pulse width modulation on GPIO 22. Frequency=100Hz and OFF
+pR = GPIO.PWM(25, 100)
+pR.start(0)
+
+redIntensity = (sumThingGroupA/sumOfThings) * 100
+greenIntensity = (sumThingGroupB/sumOfThings) * 100
+
+# Update lighting
+pG.ChangeDutyCycle(greenIntensity)
+pR.ChangeDutyCycle(redIntensity)
+
+time.sleep(120)
+
+except KeyboardInterrupt:
+    pG.stop()
+    pR.stop()
+    GPIO.cleanup()
+    quit()
