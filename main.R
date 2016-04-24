c7db61f23e3195fc93aae8868cedbba02327cabc
Bryan Shepherd <bryan.shepherd@windsorcircle.com>
2016-04-24 14:24:23 -0400

    Add setStatusWorking and setStatusDone functions. Update examples.

--- main.R
+++ main.R
@@ -10,23 +10,42 @@ access_secret = user_info[4,1]
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
 setRunStatus = function(currentStatus = NULL, updatedStatus) {
-  if (is.null(currentStatus)) {
-
+  
+  if (is.null(currentStatus) || # The double or ('||') means that if the first element
+                                # evaluates FALSE it skips to the next one. This
+                                # prevent errors for objects that don't exist and such
+      (!(is.null(currentStatus)) && 
+       !(currentStatus$text == updatedStatus))) {
+    
+    cleanupTimeline()
+    
     newStatus = updateStatus(updatedStatus)
     return(newStatus)
     
-  } else if (currentStatus$text == updatedStatus) {
+  } else { # This is the path for when the passed current status matches the 
+           # updated status. This prevents uneccessary Twitter API calls
 
     return(currentStatus)
     
+  } 
+}
+
+cleanupTimeline = function() {
+  # Note that this will return success even if there are no tweets in the timeline
+  tl = userTimeline('Texxors')
+  res = lapply(tl, deleteStatus)
+  if (length(res)>0) {
+    ifelse(all(do.call(cbind, res))==TRUE, return('Succeeded'), return('Failed'))
   } else {
-    
-    cs = userTimeline('Texxors', 1)
-    deleteStatus(cs[[1]])
-    
-    newStatus = updateStatus(updatedStatus)
-    return(newStatus)
+    return("Nothing to delete")
   }
 }
 
+setStatusWorking = function() {
+  setRunStatus(updatedStatus = 'Working')
+}
+
+setStatusDone = function() {
+  setRunStatus(updatedStatus = 'Done')
+}
 
